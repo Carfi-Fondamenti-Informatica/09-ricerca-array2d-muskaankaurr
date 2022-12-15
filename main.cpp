ac86db38d3eaceccd239c94d8a1dd403e5e84f97
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-12-15 17:18:51 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+   int contatore =0;
+   char nomi[10][20];
+   inizio (nomi, 10);
+   for (int i=0; i<10;i++){
+       cin>> nomi [i];
+   }
+
+   char nomedue [1][20];
+   inizio (nomedue,1);
+   cin >> nomedue[0];
+   if (funzione(nomi, nomedue,contatore)){
+       cout << contatore << endl;
+   }else {
+       cout << "non presente"<< endl;
+   }
+    return 0;
 }
