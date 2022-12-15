ad2862e8ad99c7e6f6072a4d634d90b603c07397
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-12-15 17:21:42 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,27 @@
+bool ciao(char nomi[10][20], char nomedue [1][20], int &contatore){
+    for (int j=0; j<20; j++){
+        if (nomi[contatore][j] != nomedue[0][j]){
+            return false;
+        }else{
+            if (j == 19){
+                return true;
+            }
+        }
+    }
+}
 
+    bool funzione (char nomi[10][20], char nomedue[1][20], int &contatore){
+        for (contatore =0; contatore<10; contatore++){
+            if (ciao (nomi, nomedue, contatore)){
+                return true;
+            }
+        }return false;
+    }
+
+    void inizio (char nomi[][20], int m){
+        for (int i=0; i<m; i++){
+            for (int j=0; j<20 ; j++){
+                nomi[i][j]=' ';
+            }
+        }
+    }
