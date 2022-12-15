cac291d8ffeb2ccf67497d7d5b99c01060e09a86
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-12-15 17:20:01 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,6 @@
-
+#ifndef ESERCITAZIONE_9_LIB_H
+#define ESERCITAZIONE_9_LIB_H
+bool funzione (char [][20], char[][20], int &contatore);
+bool ciao (char [][20], char[][20], int &contatore);
+void inizio (char [][20], int m);
+#endif
